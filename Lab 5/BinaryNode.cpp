9e28875b0fe7915e6a73478a5b338f36e75bab63
Neil Williamson <nwilliamsonwork@gmail.com>
2020-12-01 11:05:09 -0600

    Completed formatting of .cpp files

--- Lab 5/BinaryNode.cpp
+++ Lab 5/BinaryNode.cpp
@@ -1,1 +1,33 @@
 #include "BinaryNode.h"
+template<class T>
+BinaryNode::BinaryNode(){	};
+
+template<class T>
+BinaryNode::BinaryNode(const T& newItem){	};
+
+template<class T>
+BinaryNode::BinaryNode(const T& newItem,
+						std::shared_ptr<BinaryNode<T>> leftChild,
+						std::shared_ptr<BinaryNode<T>> rightChild){	};
+
+template<class T>
+void BinaryNode::setItem(const T& anItem){	};
+
+template<class T>
+T BinaryNode::getItem() const{	};
+
+template<class T>
+bool BinaryNode::isLeaf() const{	};
+
+template<class T>
+auto BinaryNode::getLeftChild() const{	};
+
+template<class T>
+auto BinaryNode::getRightChild() const{	};
+
+template<class T>
+void BinaryNode::setLeftChild(std::shared_ptr<BinaryNode<T>> newLeft){	};
+
+template<class T>
+void BinaryNode::setRightChild(std::shared_ptr<BinaryNode<T>> newRight){	};
+
--- Lab 5/EmployeeInfo.cpp
+++ Lab 5/EmployeeInfo.cpp
@@ -1,1 +1,18 @@
 #include "EmployeeInfo.h"
+
+EmployeeInfo::EmployeeInfo()
+{
+};
+		
+int EmployeeInfo::getID() const
+{
+};
+std::string EmployeeInfo::getName() const
+{
+};
+bool EmployeeInfo::setID(int)
+{
+};
+bool EmployeeInfo::setName(std::string)
+{
+};
