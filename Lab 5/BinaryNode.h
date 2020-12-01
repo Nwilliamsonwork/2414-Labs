d4fed90a439eaa0b1b768bc4078ee6995185c5fd
Neil Williamson <nwilliamsonwork@gmail.com>
2020-11-30 20:27:51 -0600

    added missing files

---
+++ Lab 5/BinaryNode.h
@@ -0,0 +1,35 @@
+#ifndef BINARYNODE_H
+#define BINARYNODE_H
+
+#include <memory>
+
+
+template<class T>
+class BinaryNode
+{
+	private:
+		T item;
+		std::shared_ptr<BinaryNode<T>> leftChild;
+		std::shared_ptr<BinaryNode<T>> rightChild;
+		
+	public:
+		BinaryNode();
+		BinaryNode(const T& newItem);
+		BinaryNode(const T& newItem,
+					std::shared_ptr<BinaryNode<T>> leftChild,
+					std::shared_ptr<BinaryNode<T>> rightChild);
+		
+		void setItem(const T& anItem);
+		T getItem() const;
+		
+		bool isLeaf() const;
+		
+		auto getLeftChild() const;
+		auto getRightChild() const;
+		
+		void setLeftChild(std::shared_ptr<BinaryNode<T>> newLeft);
+		void setRightChild(std::shared_ptr<BinaryNode<T>> newRight);
+
+};
+
+#endif
--- Lab 5/EmployeeInfo.h
+++ Lab 5/EmployeeInfo.h
@@ -1,10 +1,21 @@
 #ifndef EMPLOYEEINFO_H
 #define EMPLOYEEINFO_H
 
+#include <string>
+
 class EmployeeInfo
 {
+	private:
+		int id;
+		std::string name
+		
 	public:
-	protected:
+		EmployeeInfo();
+		
+		int getID() const;
+		std::string getName() const;
+		bool setID(int);
+		bool setName(std::string);
 };
 
 #endif
